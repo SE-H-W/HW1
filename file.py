ef801abe33adc20dc19423d65fbe18bdf4a6e31e
Nirmit73 <71959011+Nirmit73@users.noreply.github.com>
2024-08-27 15:22:24 -0400

    Create file.py

---
+++ file.py
@@ -0,0 +1,11 @@
+def isPalindrome(text):
+
+    reversed_text=''
+
+    for i in range(len(text)):
+        reversed_text = text[i] + reversed_text
+    
+    if text == reversed_text:
+        return True
+    else:
+        return False
